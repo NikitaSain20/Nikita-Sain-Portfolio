fd205f4fa54006274bb07b1e41a19b0c439384d4
nikita <nikitasen664@gmail.com>
2025-09-06 21:26:09 +0530

    Improved spacing problem

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -33,7 +33,7 @@ export default function Projects() {
           </p>
         </div>
 
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-12 lg:gap-16">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-12 ">
           {projectsData.map((project: ProjectDataType, index) => (
             <Card
               key={index}
